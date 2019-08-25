5d3a5a779508f2b1f68383cc1a89d9bb0855f48a
Kalim10 <kalim.r1989@gmail.com>
2019-08-25 22:18:38 +0200

    alles werkt :)

--- src/main/java/myfirstproject/maketacos/Taco.java
+++ src/main/java/myfirstproject/maketacos/Taco.java
@@ -1,4 +1,13 @@
 package myfirstproject.maketacos;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
 public class Taco {
+    private String name;
+    private String wrap;
+    private ArrayList<String> ingredients;
+
 }
